a1ed7a557aadc93d0b162accd3a0ca59e16a11af
Piyush Pandole <91437874+Peeyu5h@users.noreply.github.com>
2025-07-09 14:55:16 +0530

    Add files via upload

---
+++ Assets/js/main.js
@@ -0,0 +1,70 @@
+
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', ()=>{
+        navMenu.classList.add('show-menu')
+    });
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('show-menu');
+    });
+}
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*==================== ACCORDION SKILLS ====================*/
+
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header');
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className;
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close';
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open';
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+});
+
+
+function scrollHeader(){
+    const scrollY = window.pageYOffset;
+    const nav = document.getElementById('header');
+    if (this.scrollY >= 80){
+        nav.classList.add('scroll-header');
+    } else{
+        nav.classList.remove('scroll-header');
+    }
+}
+window.addEventListener('scroll', scrollHeader);
+/*==================== SHOW SCROLL UP ====================*/ 
+
+function scrollup(){
+    const scrollY = window.pageYOffset;
+    const scrollup = document.getElementById('scroll-up');
+    if(this.scrollY >= 560) scrollup.classList.add('show-scroll'); else scrollup.classList.remove('show-scroll');
+}
+
+window.addEventListener('scroll', scrollup);
